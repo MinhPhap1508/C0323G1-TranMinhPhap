da71e0b7d1398615873ecd5688b93036b2467753
MinhPhap1508 <minhphapclt@gmail.com>
2023-04-07 21:09:38 +0700

    bai tap

---
+++ ss6_javascript/thuc_hanh/bien.js
@@ -0,0 +1,18 @@
+//let i = 10
+//let f = 20.5
+//let b = true
+//let s = 'Hà Nội'
+
+//document.write('i = ' + i)
+//document.write('<br/>')
+//document.write('f = ' + f)
+//document.write('<br/>')
+//document.write('b = ' + b)
+//document.write('<br/>')
+//document.write('s = ' + s)
+
+//let width= 1000
+//let height= 500
+//let area= width*height
+//document.write('Area='+ area)
+
---
+++ ss7_bien_kieu_du_lieu_va_toan_tu/thuc_hanh/bien_toan_tu.js
@@ -0,0 +1,10 @@
+let inputRộng;
+let inputdài;
+
+inputRộng=prompt("Nhập chiều rộng");
+inputdài=prompt("Nhập chiều dài");
+
+let width=parseInt(inputRộng);
+let height=parseInt(inputdài);
+let area=width*height
+document.write("Kết quả là: " +area)
